d77811d9732bd224d50da1d19c733f6e4a0ebfbf
Yannik Hahn <yannik@h4hn.de>
2025-07-14 23:04:44 +0200

    split cms and ui

---
+++ config/plugins.ts
@@ -0,0 +1,14 @@
+export default () => ({
+  upload: {
+    config: {
+      breakpoints: {
+        xl: 1920,
+        large: 1000,
+        medium: 750,
+        small: 500,
+        xs: 256,
+        tiny: 64,
+      },
+    },
+  },
+});
